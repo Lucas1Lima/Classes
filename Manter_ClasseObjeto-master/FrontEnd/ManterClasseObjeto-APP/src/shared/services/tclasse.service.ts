09a14c61ff4c178388382d1a5d20f91b86e7941a
Edmar Prado <edmarssouzap@gmail.com>
2021-12-01 16:50:38 -0300

    Front Service

--- Manter_ClasseObjeto-master/FrontEnd/ManterClasseObjeto-APP/src/shared/services/tclasse.service.ts
+++ Manter_ClasseObjeto-master/FrontEnd/ManterClasseObjeto-APP/src/shared/services/tclasse.service.ts
@@ -2,22 +2,33 @@ import { TClasse } from './../models/TClasse';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment} from 'src/environments/environment.prod';
-import { ClassMethod } from '@angular/compiler';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class TclasseService {
 
-  constructor(public http: HttpClient) { }
-  //formulario: TClasse = new TClasse();
-  //listar: TClasse[];
+  constructor(
+    public http: HttpClient
+  ) { }
+
+  formulario: TClasse = new TClasse();
+  listar: TClasse[];
+
+  atualizarLista() {
+    this.obterClasses();
+  }
 
   deletarClasse (id: number) {
-    return this.http.delete(environment.EnderecoAPI + '' + id);
+    return this.http.delete(environment.EnderecoAPI + '/TClasseLogs/' + id);
   }
 
   obterClasses() {
-    return this.http.get(environment.EnderecoAPI + '');
+    return this.http.get(environment.EnderecoAPI + '/TClasseLogs');
   }
-/* pAPI + ;adicionarpostid:number (environment.EnderecoAPIid: number + ''atualizar()){}thisthis.http.http.get.get()enenvironment..EnderecoAPI.._isScalartotoPromise()).then.then()res => {}}thisthis.listar = res as TClasse[]];;;; + */
+
+  editarClasse(id: number) {
+    return this.http.get(environment.EnderecoAPI + '/TClasseLogs/' + id)
+  }
+}
