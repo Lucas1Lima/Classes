77d02d1d0a562ae8f0a27ee1d00e9bd6d6902af1
jonatassouta <jonatas.souta81@gmail.com>
2021-12-01 17:18:39 -0300

    cad

--- Manter_ClasseObjeto-master/FrontEnd/ManterClasseObjeto-APP/src/app/app-routing.module.ts
+++ Manter_ClasseObjeto-master/FrontEnd/ManterClasseObjeto-APP/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
+import { CadastroComponent } from './cadastro/cadastro.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "cadatro", component: CadastroComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
