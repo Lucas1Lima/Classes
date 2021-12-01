ac064fdf769bb97a9edbd054c96780f356771a66
Lucas Lima <acdacd1212@gmail.com>
2021-12-01 16:03:43 -0300

    back

---
+++ Manter_ClasseObjeto-master/BackEnd/Manter_ClasseObjeto-API/WebApplication1/WebApplication1/Models/TClasseLog.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class TClasseLog
+    {
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required, DataType(DataType.Date)]
+        public DateTime Data { get; set; }
+
+        [Required, StringLength(50)]
+        public string UsuarioResponsavel { get; set; }
+
+        [Required, StringLength(200)]
+        public string Modificacoes { get; set; }
+    }
+}
